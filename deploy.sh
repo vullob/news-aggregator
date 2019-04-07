42171e925fae901dacb8c6cc4bbe35dadca0cbe3
Brian Vullo <vullo.b@husky.neu.edu>
2019-04-07 18:03:24 -0400

    deployment files

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export MIX_ENV=prod
+export PORT=4797
+export NODEBIN=`pwd`/assets/node_modules/.bin
+export PATH="$PATH:$NODEBIN"
+
+mix deps.get
+mix compile
+(cd assets && npm install)
+(cd assets && webpack --mode production)
+mix phx.digest
+mix release
+echo "Starting app..."
+
+_build/prod/rel/news/bin/news foreground
+
---
+++ start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export MIX_ENV=prod
+export PORT=4797
+
+echo "Stopping old copy of app, if any..."
+
+_build/prod/rel/news/bin/news stop || true
+
+echo "Starting app..."
+
+
+# Foreground for testing and for systemd
+_build/prod/rel/news/bin/news foreground
+
+
+
